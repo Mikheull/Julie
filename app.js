86a79a50d6be6f6c59b8509baaf8e24e12da569a
Mikheull <mikhael.bailly1@orange.fr>
2019-10-22 12:31:29 +0200

    base structure

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const session = require('express-session');
+
+// Config
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(session({
+    secret: "N€$=vD6€HxP6vgvU33h2LZ96!;3Q4tm>mRµ",
+    saveUninitialized: true,
+    resave: true
+}))
+app.use((req, res, next) => {
+    next();
+});
+
+
+
+// Router
+const routes = require('./routes/routes');
+app.use('/', routes);
+
+
+
+app.listen(3000, () => {
+  console.log(`Listening to requests on http://localhost:3000`);
+});
